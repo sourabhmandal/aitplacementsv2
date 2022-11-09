8cfa3a3d5f1079c7b33c36259b46d8cf64dfc644
sourabhmandal <19mandal97@gmail.com>
2022-11-09 11:09:28 +0530

    add cors for all

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,18 +1,21 @@
 import * as trpcNext from "@trpc/server/adapters/next";
 import { createContext } from "../server/createContext";
 import { appRouter } from "../server/routes/app.router";
+import { withCors } from "./cors";
 
-export default trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext: createContext,
-  onError({ error }) {
-    // "BAD_REQUEST" | "INTERNAL_SERVER_ERROR" | "UNAUTHORIZED" | "FORBIDDEN" | "NOT_FOUND" | "METHOD_NOT_SUPPORTED" | "TIMEOUT" | "CONFLICT" | "PRECONDITION_FAILED"
-    if (error.code === "INTERNAL_SERVER_ERROR") {
-      console.log(`ERROR ${error.code} :: (${error.name}) ${error.message}`);
-    } else if (error.code === "PARSE_ERROR") {
-      console.log(`ERROR ${error.code} :: (${error.name}) ${error.message}`);
-    } else {
-      console.log(`ERROR ${error.code} :: (${error.name}) ${error.message}`);
-    }
-  },
-});
+export default withCors(
+  trpcNext.createNextApiHandler({
+    router: appRouter,
+    createContext: createContext,
+    onError({ error }) {
+      // "BAD_REQUEST" | "INTERNAL_SERVER_ERROR" | "UNAUTHORIZED" | "FORBIDDEN" | "NOT_FOUND" | "METHOD_NOT_SUPPORTED" | "TIMEOUT" | "CONFLICT" | "PRECONDITION_FAILED"
+      if (error.code === "INTERNAL_SERVER_ERROR") {
+        console.log(`ERROR ${error.code} :: (${error.name}) ${error.message}`);
+      } else if (error.code === "PARSE_ERROR") {
+        console.log(`ERROR ${error.code} :: (${error.name}) ${error.message}`);
+      } else {
+        console.log(`ERROR ${error.code} :: (${error.name}) ${error.message}`);
+      }
+    },
+  })
+);
