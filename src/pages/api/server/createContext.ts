2f519691203a7b61b2f251e1b740c9787c7354af
sourabhmandal <19mandal97@gmail.com>
2022-11-09 13:31:15 +0530

    add cors for all

--- src/pages/api/server/createContext.ts
+++ src/pages/api/server/createContext.ts
@@ -11,7 +11,7 @@ export function createContext({
   // enable CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Request-Method", "*");
-  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
   res.setHeader("Access-Control-Allow-Headers", "*");
 
   // accepts OPTIONS
