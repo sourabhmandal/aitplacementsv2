bb2918e843a03514a1ff20d86c47061cbcadbb18
sourabhmandal <19mandal97@gmail.com>
2022-11-09 18:43:15 +0530

    fix cors issue

--- src/pages/api/server/routes/app.router.ts
+++ src/pages/api/server/routes/app.router.ts
@@ -4,6 +4,22 @@ import { noticeRouter } from "./notice.router";
 import { userRouter } from "./user.router";
 
 export const appRouter = createRouter()
+  .middleware(async ({ ctx, next }) => {
+    ctx?.res.setHeader("Access-Control-Allow-Credentials", "true");
+    ctx?.res.setHeader(
+      "Access-Control-Allow-Origin",
+      ctx?.req?.headers.host?.toString() || "*"
+    );
+    ctx?.res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+
+    // accept options
+    if (ctx?.req.method == "OPTIONS") ctx.res.writeHead(200);
+
+    return next();
+  })
   .merge("user.", userRouter)
   .merge("notice.", noticeRouter)
   .merge("attachment.", attachmentRouter);
