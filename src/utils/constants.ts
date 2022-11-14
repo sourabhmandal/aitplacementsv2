0c4901d66922879fb8305075989ca704041d4b64
sourabhmandal <19mandal97@gmail.com>
2022-11-14 20:15:01 +0530

    add proper url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -24,6 +24,7 @@ export const AZURE_AD_TENANT_ID = process.env.AZURE_AD_TENANT_ID;
 
 export const JWT_TOKEN = process.env.JWT_TOKEN!;
 
-export const HOSTED_VERCEL_URL = `${process.env.NEXT_PUBLIC_VERCEL_URL}`;
-
-console.log(HOSTED_VERCEL_URL);
+export let HOSTED_VERCEL_URL =
+  process.env.NODE_ENV === "development"
+    ? `${process.env.NEXT_PUBLIC_SITE_URL}`
+    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
