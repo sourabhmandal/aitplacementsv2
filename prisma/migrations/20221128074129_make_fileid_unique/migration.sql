b163fbc2329fcf406c182f19e7b8a91cbb287f33
sourabhmandal <19mandal97@gmail.com>
2022-11-29 02:32:54 +0530

    remove migrations

--- prisma/migrations/20221128074129_make_fileid_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[fileid]` on the table `attachments` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "attachments_fileid_key" ON "attachments"("fileid");
