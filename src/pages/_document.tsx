1b61c33a5e942dead18db5d4a9769bbd409c1a10
sourabhmandal <19mandal97@gmail.com>
2022-12-11 00:20:25 +0530

    add document to fix layout shift

---
+++ src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import { createGetInitialProps } from "@mantine/next";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+const getInitialProps = createGetInitialProps();
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps;
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>AIT Placements</title>
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
