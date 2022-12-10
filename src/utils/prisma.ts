fdc24da890283df54093f8ae42941496be678777
sourabhmandal <19mandal97@gmail.com>
2022-12-10 14:37:05 +0530

    prisma added

---
+++ src/utils/prisma.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+import {
+  PrismaClientInitializationError,
+  PrismaClientKnownRequestError,
+  PrismaClientRustPanicError,
+  PrismaClientUnknownRequestError,
+  PrismaClientValidationError,
+} from "@prisma/client/runtime";
+import { handlePrismaError } from "./prisma.errors";
+
+declare global {
+  var prisma: PrismaClient;
+}
+export let prisma: PrismaClient;
+
+(() => {
+  try {
+    prisma = global.prisma || new PrismaClient();
+    if (process.env.NODE_ENV != "production") {
+      global.prisma = prisma;
+    }
+  } catch (e) {
+    if (
+      e instanceof PrismaClientKnownRequestError ||
+      e instanceof PrismaClientUnknownRequestError ||
+      e instanceof PrismaClientValidationError ||
+      e instanceof PrismaClientRustPanicError ||
+      e instanceof PrismaClientInitializationError
+    )
+      handlePrismaError(e);
+  }
+})();
