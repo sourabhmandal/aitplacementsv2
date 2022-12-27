6f7675e759dd0f37c25f974ed782a4c1f4be38b1
sourabhmandal <19mandal97@gmail.com>
2022-12-27 06:32:15 +0530

    add edge

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -3,6 +3,10 @@ import * as trpcNext from "@trpc/server/adapters/next";
 import { createContext } from "../../../server/context";
 import { appRouter } from "../../../server/router/_app";
 
+export const config = {
+  runtime: "experimental-edge",
+};
+
 // export API handler
 export default trpcNext.createNextApiHandler({
   router: appRouter,
