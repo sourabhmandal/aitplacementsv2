5457e28a5fd2d0205bdffe4ce13b90164aa9c28b
sourabhmandal <19mandal97@gmail.com>
2023-01-20 20:01:52 +0530

    add docker file for deploy

--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -1,16 +1,18 @@
+#!/bin/sh
+
 # get number parts
 VNUM1=${CURRENT_VERSION_PARTS[0]}
 VNUM2=${CURRENT_VERSION_PARTS[1]}
 VNUM3=${CURRENT_VERSION_PARTS[2]}
 
 
-if [[ $VERSION == 'major']]
+if [ $VERSION == 'major']
 then
   VNUM1=v$((VNUM1+1))
-elif [[ $VERSION == 'minor']]
+elif [ $VERSION == 'minor']
 then
   VNUM2=$((VNUM2+1))
-elif [[ $VERSION == 'patch' ]]
+elif [ $VERSION == 'patch' ]
 then
   VNUM3=$((VNUM3+1))
 else
